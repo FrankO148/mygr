e08e746a7ad928aa8d56344f1c89f627364e82e7
Franco Ricchi <francoricchi@gmail.com>
2014-07-02 20:18:37 -0300

    change show mode error headers

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   delete '/processes/:pid' => 'procesos#kill'
   put '/processes/:pid' => 'procesos#prioritize'
   post '/processes/' => 'procesos#create_pr'
+  match "*path", to: "application#catch_404", via: :all
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
